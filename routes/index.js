2cac7a10cae1c19a5c7bd4729465d0500186cb98
shankalpa <shan.kalp.x@gmail.com>
2016-10-09 14:31:29 -0500

    FirstCommit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+﻿var express = require('express');
+var inventors = require('../inventors.json')
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index.html', { title: 'Inventors', data : inventors});
+});
+
+module.exports = router;
